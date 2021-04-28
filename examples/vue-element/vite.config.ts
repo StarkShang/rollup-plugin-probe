cd4790f28caf976b1dd00a08026743214fef8b91
StarkShang <stark.shang@outlook.com>
2021-04-28 18:57:49 +0800

    feat: add vue element example

---
+++ examples/vue-element/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import probe from "../../src";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        {
+            ...probe({
+                hooks: {
+                    options: {},
+                    buildStart: {},
+                    transform: {},
+                    buildEnd: {},
+                    outputOption: {},
+                    renderStart: {},
+                    renderChunk: {},
+                    generateBundle: {},
+                    writeBundle: {},
+                }
+            }),
+            enforce: "pre"
+        }
+    ]
+})
